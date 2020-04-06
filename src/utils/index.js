2e821752c91f9718ef588ec2a887fff67fc45a44
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2020-04-06 14:44:43 +0200

    test: improve test coverage

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,12 @@
-export { uuid } from './uuid';
-export { linearEase } from './linearEase';
-export { getPath } from './getPath';
-export { getPathTotalLength } from './getPathTotalLength';
-export { getWrapperStyle, getTimeStyle, svgStyle, visuallyHidden } from './styles';
-export { getColorsRGB, getNormalizedColors, getStroke, colorsValidator } from './colors';
+export { uuid } from "./uuid";
+export { linearEase } from "./linearEase";
+export { getPath } from "./getPath";
+export { getPathTotalLength } from "./getPathTotalLength";
+export {
+  getWrapperStyle,
+  getTimeStyle,
+  svgStyle,
+  visuallyHidden
+} from "./styles";
+export { getColorsRGB, getNormalizedColors, getStroke } from "./colors";
+export { colorsValidator } from "./colorsValidator";
