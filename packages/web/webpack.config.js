a88c8c8a89a4a794265d333c672be02c5409bc8c
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2021-01-15 09:30:12 +0100

    feat(web): add browser list to package.json

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -1,6 +1,3 @@
 const webpackBaseConfig = require('../../webpack-base-config')
 
-module.exports = {
-  ...webpackBaseConfig,
-  target: ['web', 'es5'], // Support IE11 and old browsers
-}
+module.exports = webpackBaseConfig
